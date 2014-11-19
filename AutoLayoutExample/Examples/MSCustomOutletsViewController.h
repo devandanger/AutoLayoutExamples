69e8f005263521ecf165bb2463202341673a69c3
Evan <devandanger@gmail.com>
2014-11-19 01:21:32 -0500

    Initial revision

---
+++ AutoLayoutExample/Examples/MSCustomOutletsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PGRCustomOutletsViewController.h
+//  AutoLayoutExample
+//
+//  Created by Evan Anger on 11/19/14.
+//  Copyright (c) 2014 Evan Anger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSCustomOutletsViewController : UIViewController
+
+@end
